538ae5fc3d9e0b03180ba3fa22c86d6cb93dadf3
Ilias Ismanalijev <hello@illyism.com>
2015-06-06 22:43:52 +0000

    init

---
+++ markade.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "name": "Sample Markade Site",
+  "directories": {
+    "output": "public",
+    "template": "templates",
+    "data": "data"
+  },
+  "templates": {
+    "index.jade": [
+      "index.md",
+      "second.md"
+    ]
+  }
+};
